bc3841b56449b8bf52ca92177d3d3d4c7677ba2d
hsenmrad <husseinmrad.business@gmail.com>
2025-02-21 18:36:04 +0200

    Remaining

--- nabiha.py
+++ nabiha.py
@@ -1,9 +1,17 @@
 
 #Nabiha Salary
-Salary=float(input("Enter your Salary :"))
+salary=float(input("Enter your Salary :"))
 Month=input("En(ter the name of Month:")
 
-Saving=float(input("Enter Your Saving percentage:"))
-Rent=float(input("Enter Your Rent percentage:"))
-Electricity=float(input("Enter Your Rent percentage:"))
+saving=float(input("Enter Your Saving percentage:"))
+rent=float(input("Enter Your Rent percentage:"))
+electricity=float(input("Enter Your Rent percentage:"))
 # Second Commit
+
+allocating_savings=(saving/100)*salary
+allocating_rent=(rent/100)*rent
+allocating_electricity=(electricity/100)*electricity
+
+total_allocating = allocating_rent +allocating_electricity + allocating_savings
+Remaining_Saving=salary-total_allocating
+#Remaining Saves
