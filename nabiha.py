cc24d6cfab4ee04a4aa532474c8c69a817b4fc57
hsenmrad <husseinmrad.business@gmail.com>
2025-02-21 18:28:40 +0200

    Second Commit

---
+++ nabiha.py
@@ -0,0 +1,9 @@
+
+#Nabiha Salary
+Salary=float(input("Enter your Salary :"))
+Month=input("En(ter the name of Month:")
+
+Saving=float(input("Enter Your Saving percentage:"))
+Rent=float(input("Enter Your Rent percentage:"))
+Electricity=float(input("Enter Your Rent percentage:"))
+# Second Commit
